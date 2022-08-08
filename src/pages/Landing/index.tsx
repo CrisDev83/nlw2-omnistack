a144f356ab4839d2babddd89b4ddeb5938a6549c
CrisDev83 <cristhian.dev83@gmail.com>
2022-08-08 18:09:42 -0300

    Atualização Css Page teacher list

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
 
 import logoImg from '../../assets/imagens/logo.svg';
 import landingImg from '../../assets/imagens/landing.svg';
@@ -20,15 +22,15 @@ function Landing() {
                 <img src={landingImg} alt="Imagem Principal" className="hero-image" />
 
                 <div className="buttons-container">
-                    <a href="/" className="study">
+                    <Link to="/study" className="study">
                     <img src={studyIcon} alt="Estudar" /> Estudar
                         
-                    </a>
+                    </Link>
 
-                    <a href="/" className="give-classes">
+                    <Link to="/give-classes" className="give-classes">
                         <img src={giveClassesIcon} alt="Dar aulas" /> Dar Aulas
                         
-                    </a>
+                    </Link>
                 </div>
 
                 <span className="total-connections">
