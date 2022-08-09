31b4dec2e84d3c29aa43ae88373a244d809869fc
CrisDev83 <cristhian.dev83@gmail.com>
2022-08-08 23:56:35 -0300

    Atualização TeacherList Desktop

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
+import whatsIcon from '../../assets/imagens/icons/whatsapp.svg'
 
 
 import './styles.css'
@@ -24,6 +25,34 @@ function TeacherList() {
                     </div>
                 </form>
             </PageHeader>
+
+            <main>
+                <article className="teacher-item">
+                    <header>
+                        <img src="https://avatars.githubusercontent.com/u/2254731?v=4" alt="Diego Fernandes" />
+                        <div>
+                            <strong>Diego Fernandes</strong>
+                            <span>Química</span>
+                        </div>
+                    </header>
+                    <p>
+                        Entusiasta das melhores tecnologias de química avançada.
+                        <br /> <br />
+                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já participaram de uma das minhas explosões.
+                    </p>
+
+                    <footer>
+                        <p>
+                            Preço/hora
+                            <strong>R$ 80,00</strong>
+                        </p>
+                        <button type="button">
+                            <img src={whatsIcon} alt="Whatsapp" />
+                            Entrar em contato
+                        </button>
+                    </footer>
+                </article>
+            </main>
        </div>
     )
 }
